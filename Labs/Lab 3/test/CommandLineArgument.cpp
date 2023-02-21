500d0b0f504c5b8621115f290a5ebb3921ff0770
Lê Minh Quân <marucube35@gmail.com>
2023-02-21 15:00:50 +0700

    init repo

---
+++ Labs/Lab 3/test/CommandLineArgument.cpp
@@ -0,0 +1,11 @@
+#include "../include/Sorting.h"
+#include "../include/Data.h"
+#include "../include/cmd.h"
+
+//Main process of command line argument
+int main(int argc, const char* argv[])
+{
+    commandLineProcess(argc, argv);
+    system("pause");
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Data Structures and Algorithms
+This repository contains a collection of data structures and algorithms implemented in C/C++
